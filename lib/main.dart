e9feb96e882c7313fd1ad8bcb2547c634f0982e3
SEGVeenstra <seg.veenstra@gmail.com>
2025-04-02 20:51:15 +0200

    Update url strategy

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:go_router_02/news_app.dart';
 import 'package:go_router_02/news_app_router.dart';
 
 void main() {
+  usePathUrlStrategy();
   final router = NewsAppRouter();
 
   router.routerDelegate.addListener(() {
